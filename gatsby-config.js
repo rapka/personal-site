eab78371455658bbd6828e5696455321b4268e58
rapka <richard.adjogah@gmail.com>
2022-06-11 17:31:48 -0400

    Clean up gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `My Gatsby Site`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: 'College Hill',
+    siteUrl: 'https://www.collegehill.io',
   },
-  plugins: ["gatsby-plugin-sass", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+  plugins: ['gatsby-plugin-sass', 'gatsby-plugin-react-helmet', 'gatsby-plugin-sitemap', {
     resolve: 'gatsby-plugin-manifest',
     options: {
-      "icon": "src/images/icon.png"
+      icon: 'src/images/favicon.png',
     }
-  }]
+  }],
 };
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,54 +1,37 @@
-const path = require(`path`);
+const path = require('path');
 const fs = require('fs');
-const each = require('lodash/each')
+const each = require('lodash/each');
 
 const photos = JSON.parse(fs.readFileSync(path.join(__dirname, 'src/photos.json')));
 const musicData = JSON.parse(fs.readFileSync(path.join(__dirname, 'src/musicData.json')));
 
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions;
-  const galleryPage = path.resolve(`src/components/virtual-photography/GalleryPage.js`);
-  const releasePage = path.resolve(`src/components/music/ReleasePage.js`);
-  // Query for markdown nodes to use in creating pages.
+  const galleryPage = path.resolve('src/components/virtual-photography/GalleryPage.js');
+  const releasePage = path.resolve('src/components/music/ReleasePage.js');
+  each(photos.galleries, (gallery, key) => {
+    createPage({
+      // Path for this page — required
+      path: `/virtual-photography/${key}`,
+      component: galleryPage,
+      context: {
+        // path: key,
+      },
+    })
+  });
 
-  console.log('lala', JSON.stringify(photos.galleries.length, null, 2));
-
-    // Create photogallery pages
-    each(photos.galleries, (gallery, key) => {
-      console.log('lalala', key);
-      createPage({
-        // Path for this page — required
-        path: `/virtual-photography/${key}`,
-        component: galleryPage,
-        context: {
-          // path: key,
-          // Add optional context data to be inserted
-          // as props into the page component.
-          //
-          // The context data can also be used as
-          // arguments to the page GraphQL query.
-          //
-          // The page "path" is always available as a GraphQL
-          // argument.
-        },
-      })
-    });
-
-    each(musicData, (aliasData, alias) => {
-      each(aliasData, (releasesData, releaseType) => {
-        each(releasesData, (release, i) => {
-          createPage({
-            // Path for this page — required
-            path: `/music/${alias}/{releaseType}/${release.slug}`,
-            component: releasePage,
-            context: {
-              releaseData: release,
-            },
-          });
+  each(musicData, (aliasData, alias) => {
+    each(aliasData, (releasesData, releaseType) => {
+      each(releasesData, (release, i) => {
+        createPage({
+          // Path for this page — required
+          path: `/music/${alias}/{releaseType}/${release.slug}`,
+          component: releasePage,
+          context: {
+            releaseData: release,
+          },
         });
       });
-      console.log('lalala', alias);
-
     });
-
+  });
 };
