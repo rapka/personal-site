00c92fd12499ba992f183e9aff546bbd07e439aa
rapka <richard.adjogah@gmail.com>
2022-12-25 19:01:28 -0500

    cleanup

--- fullstack/server/util/sort.js
+++
@@ -1,6 +0,0 @@
-const sortByCreation = (items) =>
-  items.slice().sort((a, b) => a.createdAt - b.createdAt);
-
-module.exports = {
-  sortByCreation,
-};
--- fullstack/src/components/Select.jsx
+++
@@ -1,9 +0,0 @@
-import classnames from "classnames";
-import React from "react";
-import styles from "./Select.module.css";
-
-const Select = ({ className, ...props }) => (
-  <select className={classnames(styles.select, className)} {...props} />
-);
-
-export default Select;
