5acd448c3bf434459430f050b5f2f55f528462fc
rapka <richard.adjogah@gmail.com>
2022-12-25 19:32:42 -0500

    cleanup

--- fullstack/src/components/Label.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styles from "./Label.module.css";
-
-const Label = ({ children }) => (
-  <label className={styles.label}>{children}</label>
-);
-
-export default Label;
